3473085efd201edbff65b33e4164131903cc23ed
zaid.mubbashar <zaid.mubbashar@mytm.co>
2025-12-18 14:36:29 +0500

    Remove footer text

--- src/components/resume/FooterSection.tsx
+++ src/components/resume/FooterSection.tsx
@@ -1,13 +1,5 @@
 const FooterSection = () => {
-  return (
-    <footer className="py-6">
-      <div className="container mx-auto px-6">
-        <p className="text-center text-muted-foreground text-sm">
-          Made by Zaid Mubbasher , Pay Rs 1000 or Dajaj Broast to remove this footer
-        </p>
-      </div>
-    </footer>
-  );
+  return null;
 };
 
 export default FooterSection;
